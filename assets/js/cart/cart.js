56559a8574727bef940322b140d8bb2e169d72e1
Richa Sharma <Richasharma@BHAVNACORP.NET>
2018-03-31 17:25:42 +0530

    localstorage done

--- assets/js/cart/cart.js
+++ assets/js/cart/cart.js
@@ -4,31 +4,51 @@ try {
 		// store product info in local storage if user is not logged in
 		var cartLocalStorage = function (e, el) {
 			e.preventDefault();
-			var $form = el.closest($('form')),
-				$span = $form.find($('span')),
-				i = 0,
-				$values = [];
-			$span.each(function(){
-				var $this = $(this),
-					obj = {};
-					obj.name = $this.attr('data-value');
-					obj.value = $this.text();
-				$values[i] = obj;
-				i++;
-			});
+			// localStorage.removeItem('cart');
+			var $pId = $('.product-id').text(),
+				$qty = $('.qty').val(),
+				$obj = {},
+				$arr = [],
+				$key = JSON.parse(localStorage.getItem('cart')),
+				$counter = false;
+				
+			$obj = {
+				'id' : $pId,
+				'qty' : $qty
+			};
 
-			// console.log($values);
+			if($key){
+				$.each($key, function(index, value){
+					if(value.id == $pId) {
+						$qty = parseInt($qty) + parseInt(value.qty);
+						value.qty = $qty;
+						$key[index] = {'id' : value.id, 'qty' : value.qty};
+						$counter = true;
+					}						
+				});
 
-			// if(localStorage){
-			// 	localStorage.setItem($values.name, $values.value);
-			// }
+				if($counter == true) {
+					localStorage.setItem('cart', JSON.stringify($key));
+					console.log($key);
+				}
+				else {
+					$key.push($obj);
+					localStorage.setItem('cart', JSON.stringify($key));
+					console.log($key);			
+				}
+			}
+			else {
+				$arr.push($obj);
+				localStorage.setItem('cart', JSON.stringify($arr));
+				var abc = localStorage.getItem('cart');
+				console.log($key);
+			}
 		};
 
 		// cart view from local storage
 		var cartLocalView = function(){
-			// console.log('111');
 			if(localStorage){
-				// console.log(localStorage.getItem('id', 'price'));	
+				console.log(localStorage.getItem('id', 'price'));	
 			}
 		};
 		
@@ -37,13 +57,13 @@ try {
 			$(document).on('click', '.cart-add', function(e){
 				var $this = $(this);
 				cartLocalStorage(e, $this);
-				$this.closest($('form')).submit();
+				// $this.closest($('form')).submit();
 			});
 
 
 
 			//functions to call on page load will come here 
-			cartLocalView();
+			// cartLocalView();
 		});
 
 		window.cartLocalStorage = cartLocalStorage;
